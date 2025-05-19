3bc506508fc686d97e417b8cdef68599edb3dcc2
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-05-19 14:38:20 +0300

    Fix import path for SearchBar component and clean up commented imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,14 @@
 
 import ImageGallery from './ImageGallery/ImageGallery';
-// import ImageCard from './ImageCard';
 // import ImageModal from './ImageModal';
 // import ImagePreview from './ImagePreview';
 import ErrorMessage from './ErrorMessage/ErrorMessage';
-import SearchBar from './Searchbar/SearchBar';
+import SearchBar from './SearchBar/SearchBar';
 // import LoadMoreBtn from './LoadMoreBtn';
 
 
 
 export {
-    // ImageCard,
     ImageGallery,
     // ImageModal,
     // ImagePreview,
